7dd27c151c6ee71014c4a1ea0529068f28823982
sudhanshu282006 <122244449+sudhanshu282006@users.noreply.github.com>
2025-09-29 01:21:24 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,11 +2,11 @@ import os
 from os import environ
 
 # API Configuration
-API_ID = int(os.environ.get("API_ID", "294351"))
-API_HASH = os.environ.get("API_HASH", "2d211eb63606daee")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "24548240"))
+API_HASH = os.environ.get("API_HASH", "f64dbe9639fa7b92368b045a33110d9c")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8466364709:AAF3bZrcC31EewGXSihO0OmVJuAoIUgz3wM")
 
-CREDIT = os.environ.get("CREDIT", "⌯ FʀᴏɴᴛMᴀɴ | ×͜× |")
+CREDIT = os.environ.get("CREDIT", "ss")
 # MongoDB Configuration
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "DevThanos")
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Dev-Thanos")  # Add your own atlas db
@@ -59,6 +59,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
